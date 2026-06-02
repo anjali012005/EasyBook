96aec34c70b4fe950e5144f06e5c891bfe9bd825
ANJALI DAHARWAL <131749309+anjali012005@users.noreply.github.com>
2026-06-02 23:07:11 +0530

    Update index.js

--- init/index.js
+++ init/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const MONGO_URL = "mongodb+srv://anjalidaharwal5:<anjali2005>@cluster0.n9a5jfq.mongodb.net/?easybook=Cluster0";
 
 main().then(() => {
     console.log("connected to DB");
